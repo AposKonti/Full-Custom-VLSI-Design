5beb4e46783d802df65d821cf61e8e22be8015d2
AposKonti <49614817+AposKonti@users.noreply.github.com>
2023-12-09 01:05:43 +0200

    Add files via upload

---
+++ vlsi/cad/gf65/primelib_gf65/OR/runtime/cdpl/pid7330/run_7330.sh
@@ -0,0 +1,230 @@
+#!/bin/sh
+export CLS_CDCD_COMPATIBILITY_LOCKING='NO'
+export SYNOPSYS_HOME='/proj/cad/synopsys/synopsys'
+export MANPATH='/usr/local/man:/usr/man:/usr/share/man:/proj/cad/synopsys/synopsys/ns/doc/ns/man:/proj/cad/cadence/ic-6.17.711_lnx86/share/man:/proj/cad/cadence/ic-6.17.711_lnx86/tools/man:/proj/cad/cadence/ic-6.17.711_lnx86/share/man:/proj/cad/cadence/ic-6.17.711_lnx86/tools/man'
+export SYNOPSYS_LC_ROOT='/proj/cad/synopsys/lc_vL-2016.06-SP1'
+export NNTPSERVER='news.utdallas.edu'
+export TCAD_HOME='/proj/cad/synopsys/synopsys_2018/sentaurus_vO_2018.06'
+export QRC_HOME='/proj/cad/cadence/ext.lnx86'
+export XDG_SESSION_ID='30667'
+export SYNOPSYS_TMAX='/proj/cad/synopsys/synopsys_2018/tmax_vO-2018.06-SP1'
+# export HOSTNAME='engnx03a.utdallas.edu'
+export DBUS_STARTER_ADDRESS='unix:abstract=/tmp/dbus-CdYIB1hxIo,guid=7a53a26fd029e41341ac7d6d6564aef7'
+export MALLOC_CHECK_='0'
+export CONSTRAINTS='/home/eng/a/axk220238/vlsi/rtl/vlog/matrix_alu/constraints/constraints.tcl'
+# export SHELL='/bin/bash'
+export VTE_VERSION='5204'
+export XDG_MENU_PREFIX='gnome-'
+export NETDATA_CACHE_DIR='/var/cache/netdata'
+export __LMOD_REF_COUNT_MODULEPATH='/usr/local/lmod/modulefiles:1;/etc/modulefiles:1;/usr/share/modulefiles:1;/usr/share/modulefiles/Linux:1;/usr/share/modulefiles/Core:1;/usr/share/lmod/lmod/modulefiles/Core:1'
+export TERM='xterm-256color'
+# export HOST='engnx03a.utdallas.edu'
+export SIS_LIBRARY_PATH='/proj/cad/synopsys/synopsys_2021/primelib/T-2022.03-SP1/linux64/lib'
+export XCELIUM='/proj/cad/cadence/xcelium-18.09.001'
+export BASE='/proj/cad'
+export SKIP_CDS_DIALOG='YES'
+# export MAKEFLAGS=''
+export gf65primelibpath='/home/eng/a/axk220238/vlsi/cad/gf65/primelib_gf65_axk220238_axk230133'
+export HISTSIZE='40'
+export LMOD_ROOT='/usr/share/lmod'
+export NETDATA_VARLIB_DIR='/var/lib/netdata'
+export MODULEPATH_ROOT='/usr/share/modulefiles'
+export TMPDIR='/usr/tmp'
+export CPA='/proj/cad/cadence/CPA_08.20.140/eclair-x86_64_U12-CPA_8.2.14_160704_RC2'
+export GNOME_TERMINAL_SCREEN='/org/gnome/Terminal/screen/546eefe4_12c8_4945_a6d2_9efe6a359388'
+export CDS_LOAD_ENV='CSF'
+export LMOD_PKG='/usr/share/lmod/lmod'
+export EDIT='pico'
+export PULSE_CONFIG='/home/eng/a/axk220238/.nx/nxdevice/D-1015-AA1C908FEA6B39211552387EEC3D592F/audio/daemon.conf'
+export OLDPWD='/proj/cad/synopsys/synopsys_2021/primelib/T-2022.03-SP1/bin'
+export IOTYPE='INLINE'
+export QTDIR='/usr/lib64/qt-3.3'
+export LMOD_VERSION='8.7.7'
+export QTINC='/usr/lib64/qt-3.3/include'
+export MAGMA_ROOT='/proj/cad/synopsys/synopsys_2021/primelib/T-2022.03-SP1/talus_home1.2/linux64'
+export OS='lnx86'
+export PULSE_SERVER='/home/eng/a/axk220238/.nx/nxdevice/D-1015-AA1C908FEA6B39211552387EEC3D592F/audio/native.socket'
+export WORKSPACE='/home/eng/a/axk220238'
+export CNI_PLAT_ROOT='/proj/cad/cni/plat_linux_gcc411_64'
+export QT_GRAPHICSSYSTEM_CHECKED='1'
+export NX_SESSION_ID='AA1C908FEA6B39211552387EEC3D592F'
+export my_lib_ff='/home/eng/a/axk220238/vlsi/cad/gf65/primelib_gf65_axk220238_axk230133/my_ff.lib'
+export HISTFILESIZE='40'
+# export USER='axk220238'
+export SOC='/proj/cad/cadence/soc-8.10.004.lnx86'
+export LD_LIBRARY_PATH='/proj/cad/synopsys/synopsys_2021/primelib/T-2022.03-SP1/linux64/lib/qt5_libs/lib:/proj/cad/synopsys/synopsys_2021/primelib/T-2022.03-SP1/linux64/lib:/proj/cad/synopsys/synopsys_2021/primelib/T-2022.03-SP1/talus_home1.2/linux64/lib:/proj/cad/lib:/proj/cad/cadence/assura-615_04.14.0001.lnx86/tools/lib:/proj/cad/cadence/assura-615_04.14.0001.lnx86/tools/assura/lib:/tools/lib:/proj/cad/cadence/ic-6.17.711_lnx86/tools/lib:/proj/cad/cadence/ic-6.17.711_lnx86/tools/inca/lib:/proj/cad/lib:/proj/cad/cadence/assura-615_04.14.0001.lnx86/tools/lib:/proj/cad/cadence/assura-615_04.14.0001.lnx86/tools/assura/lib:/tools/lib:/proj/cad/cadence/ic-6.17.711_lnx86/tools/lib:/proj/cad/cadence/ic-6.17.711_lnx86/tools/inca/lib:/proj/cad/cadence/ic-6.17.711_lnx86/oa_v22.50.065/lib/linux_rhel40_64/opt:/proj/cad/cadence/ic-6.17.711_lnx86/oa_v22.50.065/lib/linux_rhel40_64/opt'
+export LMOD_sys='Linux'
+export PRIME_EXEC='primelib'
+export GNOME_TERMINAL_SERVICE=':1.249'
+export CDS='/proj/cad/cadence/ic-6.17.711_lnx86'
+export my_lib_cells='/home/eng/a/axk220238/vlsi/cad/gf65/primelib_gf65_axk220238_axk230133/my_lib_cells.lib'
+export LIB='linux_rhel40_64'
+export FILELIST='/home/eng/a/axk220238/vlsi/rtl/vlog/matrix_alu/filelist.top'
+export SSH_AUTH_SOCK='/run/user/786799/keyring/ssh'
+export DESIGN_TOP='matrix_alu'
+export SNPSLMD_LICENSE_FILE='1700@engdmv.utdallas.edu'
+export USERNAME='axk220238'
+export SESSION_MANAGER='local/unix:@/tmp/.ICE-unix/30622,unix/unix:/tmp/.ICE-unix/30622'
+export AMSHOME='/proj/cad/cadence/xcelium-18.09.001'
+export MAKELEVEL='1'
+export CNI_LOG_DEFAULT='/dev/null'
+export NX_CONNECTION='10.182.66.166 44446 10.182.66.68 4000'
+export AFS_VERSION='2015_Q4'
+export PULSE_CLIENTCONFIG='/home/eng/a/axk220238/.nx/nxdevice/D-1015-AA1C908FEA6B39211552387EEC3D592F/audio/client.conf'
+export PAGER='less'
+# export MFLAGS=''
+export SYNOPSYS_NCX_ROOT='/proj/cad/synopsys/synopsys_2021/primelib/T-2022.03-SP1'
+export ED='pico'
+export BDA_HOME='/proj/cad/bda'
+export PULSE_SCRIPT='/home/eng/a/axk220238/.nx/nxdevice/D-1015-AA1C908FEA6B39211552387EEC3D592F/audio/default.pa'
+export BEOL_STACK='6_00_01_00_LB'
+export _ModuleTable001_='X01vZHVsZVRhYmxlXyA9IHsKTVR2ZXJzaW9uID0gMywKY19yZWJ1aWxkVGltZSA9IGZhbHNlLApjX3Nob3J0VGltZSA9IGZhbHNlLApkZXB0aFQgPSB7fSwKZmFtaWx5ID0ge30sCm1UID0ge30sCm1wYXRoQSA9IHsKIi91c3IvbG9jYWwvbG1vZC9tb2R1bGVmaWxlcyIsICIvZXRjL21vZHVsZWZpbGVzIiwgIi91c3Ivc2hhcmUvbW9kdWxlZmlsZXMiCiwgIi91c3Ivc2hhcmUvbW9kdWxlZmlsZXMvTGludXgiLCAiL3Vzci9zaGFyZS9tb2R1bGVmaWxlcy9Db3JlIiwgIi91c3Ivc2hhcmUvbG1vZC9sbW9kL21vZHVsZWZpbGVzL0NvcmUiLAp9LApzeXN0ZW1CYXNlTVBBVEggPSAiL2V0Yy9tb2R1bGVmaWxlczovdXNyL3NoYXJlL21vZHVsZWZpbGVzOi91c3Ivc2hhcmUvbW9kdWxl'
+export SPECTRE_DEFAULTS='-E'
+export RELXPERTHOME='/proj/cad/cadence/mmsim_15.10.644.lnx86'
+export PATH='/proj/cad/synopsys/synopsys_2021/primelib/T-2022.03-SP1/bin:/proj/cad/synopsys/synopsys_2021/primelib/T-2022.03-SP1/linux64/bin:/proj/cad/bda/2015_Q4/afs_root/bin:/proj/cad/mentor_2014/modeltech/bin:/proj/cad/mentor_2016/ams_16.4/bin:/proj/cad/mentor_2013/ixl_cal_2013.2_18.13/bin:/proj/cad/cadence/mmsim_15.10.644.lnx86/tools/bin:/proj/cad/cadence/mmsim_15.10.644.lnx86/tools/dfII/bin:/proj/cad/cadence/innovus-17.10.000/tools/bin:/proj/cad/cadence/soc-8.10.004.lnx86/tools/bin:/proj/cad/cadence/assura-615_04.14.0001.lnx86/tools/bin:/proj/cad/cadence/assura-615_04.14.0001.lnx86/tools/assura/bin:/proj/cad/cadence/ext.lnx86/tools/bin:/proj/cad/cadence/ext.lnx86/tools/extraction/bin:/proj/cad/cadence/ic-6.17.711_lnx86/tools/bin:/proj/cad/cadence/ic-6.17.711_lnx86/tools/dfII/bin:/proj/cad/cadence/ic-6.17.711_lnx86/tools/java:/proj/cad/cadence/ic-6.17.711_lnx86/tools/java/bin:/proj/cad/cadence/xcelium-18.09.001/bin:/proj/cad/cadence/xcelium-18.09.001/tools/bin:/proj/cad/cadence/conformal_15.1.lnx86/bin:/proj/cad/cadence/CPA_08.20.140/eclair-x86_64_U12-CPA_8.2.14_160704_RC2/bin:/proj/cad/bda/2015_Q4/afs_root/bin:/proj/cad/mentor_2014/modeltech/bin:/proj/cad/mentor_2016/ams_16.4/bin:/proj/cad/mentor_2013/ixl_cal_2013.2_18.13/bin:/proj/cad/cadence/mmsim_15.10.644.lnx86/tools/bin:/proj/cad/cadence/mmsim_15.10.644.lnx86/tools/dfII/bin:/proj/cad/cadence/innovus-17.10.000/tools/bin:/proj/cad/cadence/soc-8.10.004.lnx86/tools/bin:/proj/cad/cadence/assura-615_04.14.0001.lnx86/tools/bin:/proj/cad/cadence/assura-615_04.14.0001.lnx86/tools/assura/bin:/proj/cad/cadence/ext.lnx86/tools/bin:/proj/cad/cadence/ext.lnx86/tools/extraction/bin:/proj/cad/cadence/ic-6.17.711_lnx86/tools/bin:/proj/cad/cadence/ic-6.17.711_lnx86/tools/dfII/bin:/proj/cad/cadence/ic-6.17.711_lnx86/tools/java:/proj/cad/cadence/ic-6.17.711_lnx86/tools/java/bin:/proj/cad/cadence/xcelium-18.09.001/bin:/proj/cad/cadence/xcelium-18.09.001/tools/bin:/proj/cad/cadence/conformal_15.1.lnx86/bin:/proj/cad/cadence/CPA_08.20.140/eclair-x86_64_U12-CPA_8.2.14_160704_RC2/bin:/proj/cad/synopsys/synopsys_2018/pwr_vP-2019.03-SP5/pwr/P-2019.03-SP5/bin:/proj/cad/synopsys/quantumATK-2018.06-SP1-1/bin:/usr/local/bin:/usr/bin:/bin:/usr/ccs/bin:/sbin:/usr/sbin:/usr/local/openwin/bin:/usr/openwin/bin:/proj/cad/synopsys/synopsys_2018/fpga_vN-2018.03-SP1-1/bin:/proj/cad/synopsys/synopsys_2018/identify_vN-2018.03-SP1-1/bin:/proj/cad/synopsys/synopsys_2018/syn_vO-2018.06-SP1/bin:/proj/cad/synopsys/lc_vL-2016.06-SP1/bin:/proj/cad/synopsys/synopsys_2018/vcs-mx_vO-2018.09-1/bin:/proj/cad/synopsys/synopsys_2018/tmax_vO-2018.06-SP1/bin:/proj/cad/synopsys/synopsys/simif/bin:/proj/cad/synopsys/synopsys_2018/hspice_vO-2018.09-2/hspice/bin:/proj/cad/synopsys/synopsys_2018/hsim_vO-2018.09/hsimplus/bin:/proj/cad/synopsys/synopsys_2018/cwave_vO-2018.09-SP1/bin:/proj/cad/synopsys/synopsys/ns/suse32/ns/bin:/proj/cad/synopsys/synopsys/cosmos/ai_bin:/proj/cad/synopsys/synopsys_2018/sentaurus_vO_2018.06/bin:/proj/cad/synopsys/icwbev_plus_vM-2016.12.lnx86_64/bin:/proj/cad/synopsys/synopsys_2018/pts_vO-2018.06-SP1/bin:/proj/cad/synopsys/synopsys/ncx/ncx/bin:/proj/cad/synopsys/icc2_vR-2020.09-SP5/icc/R-2020.09-SP4/bin:/proj/cad/synopsys/icc2_vR-2020.09-SP5/icc2/R-2020.09-SP5/bin:/proj/cad/synopsys/synopsys_2018/formality_vO-2018.06-SP5/bin:/proj/cad/synopsys/nanotime_vR-2020.09-SP1/bin:/proj/cad/synopsys/synopsys_2021/primelib/T-2022.03-SP1/bin:/proj/cad/synopsys/mw_vM-2016.12-SP5/bin/linux64:/proj/cad/synopsys/starrc_vM-2016.12-SP3-2/bin:/proj/cad/cadence/ic-6.17.711_lnx86/oa_v22.50.065/bin:/proj/cad/cni/plat_linux_gcc411_64/3rd/bin:/proj/cad/cni/plat_linux_gcc411_64/3rd/oa/lib/linux_rhel30_gcc411_64/opt:/proj/cad/cni/plat_linux_gcc411_64/lib:/proj/cad/cadence/ic-6.17.711_lnx86/oa_v22.50.065/bin:/proj/cad/cni/plat_linux_gcc411_64/3rd/bin:/proj/cad/cni/plat_linux_gcc411_64/3rd/oa/lib/linux_rhel30_gcc411_64/opt:/proj/cad/cni/plat_linux_gcc411_64/lib'
+export MAIL='/var/spool/mail/axk220238'
+export NETDATA_USER_CONFIG_DIR='/etc/netdata'
+export PT_HOME='/proj/cad/synopsys/synopsys_2018/pts_vO-2018.06-SP1'
+export CAL_HOME='/proj/cad/mentor_2013/ixl_cal_2013.2_18.13'
+export NXDIR='/usr/NX'
+export QT_IM_MODULE='ibus'
+export TECH='my_gf65\0011'
+export DC_EXEC='dc_shell'
+export PWD='/home/eng/a/axk220238/vlsi/cad/gf65/primelib_gf65_axk220238_axk230133'
+export DESIGN_TYPE='CELL'
+export XMODIFIERS='@im=ibus'
+export VCS_HOME='/proj/cad/synopsys/synopsys_2018/vcs-mx_vO-2018.09-1'
+export CWAVE_HOME='/proj/cad/synopsys/synopsys_2018/cwave_vO-2018.09-SP1'
+export FMHOME='/usr/local/frame'
+export AFS_ROOT='/proj/cad/bda/2015_Q4/afs_root'
+export CNI_ROOT='/proj/cad/cni'
+export EDITOR='pico'
+export CELL='OR'
+export MODULEPATH='/usr/local/lmod/modulefiles:/etc/modulefiles:/usr/share/modulefiles:/usr/share/modulefiles/Linux:/usr/share/modulefiles/Core:/usr/share/lmod/lmod/modulefiles/Core'
+export GDK_BACKEND='x11'
+export KDEDIRS='/usr'
+export NX_SYSTEM='/usr/NX'
+export _ModuleTable_Sz_='2'
+export VCS_ARCH_OVERRIDE='linux'
+export AFS_HOME='/proj/cad/bda'
+export MMSIM='/proj/cad/cadence/mmsim_15.10.644.lnx86'
+export ICC_HOME='/proj/cad/synopsys/icc2_vR-2020.09-SP5/icc/R-2020.09-SP4'
+export PYTHONHOME='/proj/cad/cni/plat_linux_gcc411_64/3rd'
+export VIRTUOSO='virtuoso'
+export IC='/proj/cad/cadence/ic-6.17.711_lnx86'
+export QT_LIBRARY_STORE='/proj/cad/synopsys/synopsys_2021/primelib/T-2022.03-SP1/linux64/lib/qt5_libs'
+export LC_EXEC='lc_shell'
+export LMOD_CMD='/usr/share/lmod/lmod/libexec/lmod'
+export FORMAL_HOME='/proj/cad/synopsys/synopsys_2018/formality_vO-2018.06-SP5'
+export CDSHOME='/proj/cad/cadence/ic-6.17.711_lnx86'
+export AMS_HOME='/proj/cad/mentor_2016/ams_16.4'
+export SYNOPSYS_BASE='/proj/cad/synopsys'
+export LM_LICENSE_FILE='5280@engdmv:5280@engdmv:1700@engdmv.utdallas.edu'
+export INNOVUS='innovus'
+export CDS_Netlisting_Mode='Analog'
+export NX_RUNNER='/usr/NX/bin/nxrunner'
+export HISTCONTROL='ignoredups'
+export SSH_ASKPASS='/usr/libexec/openssh/lxqt-openssh-askpass'
+export SIGN='/home/eng/a/axk220238/.signature'
+export var='/home/eng/a/axk220238/vlsi/flow/tcl_scripts/primelib2.tcl'
+export NX_CUPS_BIN='/usr/bin'
+export DBUS_STARTER_BUS_TYPE='session'
+export HSIM_HOME='/proj/cad/synopsys/synopsys_2018/hsim_vO-2018.09'
+export TECHDIR='/proj/cad/library/mosis/GF65_LPe/Calibre'
+export HOME='/home/eng/a/axk220238'
+# export SHLVL='3'
+export MGC_HOME='/proj/cad/mentor_2013/ixl_cal_2013.2_18.13'
+export ICC2_HOME='/proj/cad/synopsys/icc2_vR-2020.09-SP5/icc2/R-2020.09-SP5'
+export _ModuleTable002_='ZmlsZXMvTGludXg6L3Vzci9zaGFyZS9tb2R1bGVmaWxlcy9Db3JlOi91c3Ivc2hhcmUvbG1vZC9sbW9kL21vZHVsZWZpbGVzL0NvcmUiLAp9Cg=='
+export CDS_AUTO_64BIT='ALL'
+export PULSE_RUNTIME_PATH='/home/eng/a/axk220238/.nx/nxdevice/D-1015-AA1C908FEA6B39211552387EEC3D592F/audio'
+export GNOME_DESKTOP_SESSION_ID='this-is-deprecated'
+export model_loc='models/liberty/liberty_op_cond.lib'
+export BASH_ENV='/usr/share/lmod/lmod/init/bash'
+export OLDPATH='/usr/lib64/qt-3.3/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/bin:/usr/bin:/usr/X11R6/bin:/opt/puppetlabs/bin'
+export LOGNAME='axk220238'
+export PYTHONPATH='/proj/cad/synopsys/synopsys_2021/primelib/T-2022.03-SP1/linux64/lib:/proj/cad/cni/pylib:/proj/cad/cni/plat_linux_gcc411_64/pyext:/proj/cad/cni/plat_linux_gcc411_64/lib'
+export MGLS_LICENSE_FILE='1717@engdmv'
+export QTLIB='/usr/lib64/qt-3.3/lib'
+export LMOD_SHELL_PRGM='bash'
+export VISUAL='pico'
+export XDG_DATA_DIRS='/home/eng/a/axk220238/.local/share/flatpak/exports/share:/var/lib/flatpak/exports/share:/usr/local/share:/usr/share'
+export CONFORMAL='/proj/cad/cadence/conformal_15.1.lnx86'
+export DBUS_SESSION_BUS_ADDRESS='unix:abstract=/tmp/dbus-CdYIB1hxIo,guid=7a53a26fd029e41341ac7d6d6564aef7'
+export MODULESHOME='/usr/share/lmod/lmod'
+export LESSOPEN='||/usr/bin/lesspipe.sh %s'
+export VIRTUOSO_LIB_NAME='axk220238_axk230133'
+export LMOD_SETTARG_FULL_SUPPORT='no'
+export MBOX='/home/eng/a/axk220238/MAIL'
+export SYNOPSYS_SYN_ROOT='/proj/cad/synopsys/synopsys_2018/syn_vO-2018.06-SP1'
+export HSPICE_HOME='/proj/cad/synopsys/synopsys_2018/hspice_vO-2018.09-2/hspice'
+# export CDPL_HOME='/proj/cad/synopsys/synopsys_2021/primelib/T-2022.03-SP1/cdpl_runtime_linux64'
+export LEGACY_MIMCAP='NO'
+export XDG_RUNTIME_DIR='/run/user/786799'
+export DISPLAY=':1015'
+export QT_PLUGIN_PATH='/proj/cad/synopsys/synopsys_2021/primelib/T-2022.03-SP1/linux64/lib/qt5_libs/plugins/'
+export STDB='/home/eng/a/axk220238/vlsi/flow'
+export LMOD_DIR='/usr/share/lmod/lmod/libexec'
+export XDG_CURRENT_DESKTOP='GNOME'
+export NX_ROOT='/home/eng/a/axk220238/.nx'
+export SNPS_SIMIF='/proj/cad/synopsys/synopsys/simif'
+export CCI_ROOT='/proj/cad/synopsys/synopsys_2021/primelib/T-2022.03-SP1/etc/cci_root_linux64'
+export SYNOPSYS_HOME_2021='/proj/cad/synopsys/synopsys_2021'
+export LIB1='linux_rhel30_gcc411_6'
+export ASSURAHOME='/proj/cad/cadence/assura-615_04.14.0001.lnx86'
+export XAUTHORITY='/home/eng/a/axk220238/.Xauthority'
+export NETDATA_STOCK_CONFIG_DIR='/etc/netdata/conf.d'
+export COLORTERM='truecolor'
+export NX_TEMP='/tmp'
+export CDS_INST_DIR='/proj/cad/cadence/ic-6.17.711_lnx86'
+export SCL_ENABLE_SPANNING='1'
+export FLEXLM_BORROWFILE='/home/eng/a/axk220238/.engnx03a.utdallas.edu-borrow.txt'
+unset LS_COLORS
+cd /home/eng/a/axk220238/vlsi/cad/gf65/primelib_gf65_axk220238_axk230133
+estatus=$?
+if [ $estatus != 0 ]; then
+    echo Exit status $estatus
+    exit $estatus
+fi
+umask 66
+hostname=$(hostname -s)
+hostname2=$(hostname --long)
+/proj/cad/synopsys/synopsys_2021/primelib/T-2022.03-SP1/bin/primelib  -server /home/eng/a/axk220238/vlsi/cad/gf65/primelib_gf65_axk220238_axk230133/OR/runtime/cdpl/pid7330/driver_7330.tcl>&/home/eng/a/axk220238/vlsi/cad/gf65/primelib_gf65_axk220238_axk230133/OR/runtime/cdpl/pid7330/$$.out
+estatus=$?
+if [ $estatus != 0 ]; then
+    echo Exit status $estatus >> /home/eng/a/axk220238/vlsi/cad/gf65/primelib_gf65_axk220238_axk230133/OR/runtime/cdpl/pid7330/$$.out
+    exit $estatus
+fi
+workdir=$(\readlink -f /var/tmp/pl.$hostname.786799.$$)
+IFS='.' toks=( $workdir )
+sisid=${toks[${#toks[@]}-1]}
+if [ -e "$workdir" ]; then
+   \rm -vrf "$workdir/"
+fi
+shopt -s nullglob
+IFS=''
+if [ -f "/var/tmp/worker.W*.$hostname.$sisid.log" ]; then
+   \mv -vf /var/tmp/worker.W*.$hostname.$sisid.log /home/eng/a/axk220238/vlsi/cad/gf65/primelib_gf65_axk220238_axk230133/OR/runtime/cdpl/pid7330
+elif [ -f "/var/tmp/worker.W*.$hostname2.$sisid.log" ]; then
+   \mv -vf /var/tmp/worker.W*.$hostname2.$sisid.log /home/eng/a/axk220238/vlsi/cad/gf65/primelib_gf65_axk220238_axk230133/OR/runtime/cdpl/pid7330
+fi
+for sislog in /var/tmp/pl.786799.W*.$hostname.$sisid.log; do
+   if [ -f "$sislog" ]; then
+      echo "Exit status $estatus" >> "$sislog"
+      \mv -vf "$sislog" /home/eng/a/axk220238/vlsi/cad/gf65/primelib_gf65_axk220238_axk230133/OR/runtime/cdpl/pid7330
+   fi
+done
+for sislog in /var/tmp/pl.786799.W*.$hostname2.$sisid.log; do
+   if [ -f "$sislog" ]; then
+      echo "Exit status $estatus" >> "$sislog"
+      \mv -vf "$sislog" /home/eng/a/axk220238/vlsi/cad/gf65/primelib_gf65_axk220238_axk230133/OR/runtime/cdpl/pid7330
+   fi
+done
+\rm -vf /var/tmp/pl.$hostname.786799.$$
+\rm -vrf /var/tmp/primelib.$hostname.786799.$sisid
+\rm -vrf /var/tmp/primelib.$hostname2.786799.$sisid
+echo Exit status $estatus >> /home/eng/a/axk220238/vlsi/cad/gf65/primelib_gf65_axk220238_axk230133/OR/runtime/cdpl/pid7330/$$.out
+if [ $estatus == 0 ]; then
+   \rm -f /home/eng/a/axk220238/vlsi/cad/gf65/primelib_gf65_axk220238_axk230133/OR/runtime/cdpl/pid7330/$$.out
+fi
+exit $estatus
